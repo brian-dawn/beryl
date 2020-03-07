17312cd6c0febfbee908d14a5e1b96ff7ef4d9f8
Brian Dawn <brian.t.dawn@gmail.com>
2020-03-07 11:07:24 -0600

    Initialize the repo.

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use std::fs;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+use blurz::bluetooth_adapter::BluetoothAdapter;
+use blurz::bluetooth_device::BluetoothDevice;
+use blurz::bluetooth_session::BluetoothSession;
+
+fn run() -> Result<(), Box<std::error::Error>> {
+    let allowed_devices = load_devices()?;
+
+    if allowed_devices.is_empty() {
+        println!("please add some devices to ~/.config/beryl/devices");
+        std::process::exit(1);
+    }
+
+    let session = BluetoothSession::create_session(None)?;
+    let adapter = BluetoothAdapter::init(&session)?;
+
+    loop {
+        let devices = adapter.get_device_list()?;
+        for d in devices {
+
+            // Only if our allow strings match anything in this list.
+            if !allowed_devices.iter().any(|allow| d.contains(allow)) {
+                continue;
+            }
+
+            let device = BluetoothDevice::new(&session, d.to_string());
+            if !device.is_connected()? {
+                println!("connecting ...");
+                let attempt = device.connect(5000);
+                match attempt {
+                    Ok(success) => {
+                        println!("success");
+                    }
+                    Err(err) => {
+                        println!("error {:?}", err);
+                    }
+                }
+            }
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(3000));
+    }
+}
+
+fn load_devices() -> Result<Vec<String>, Box<std::error::Error>> {
+    let mut config = dirs::config_dir().ok_or("couldn't get config dir")?;
+    config.push("beryl");
+
+    if !config.exists() {
+        std::fs::create_dir_all(&config)?;
+    }
+
+    config.push("devices");
+
+    if !config.exists() {
+        std::fs::File::create(&config)?;
+    }
+
+    let mut file = std::fs::File::open(&config)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+
+    Ok(contents
+        .lines()
+        .into_iter()
+        .map(|line| line.trim().to_owned())
+        .collect())
+}
+
+fn main() {
+    run().unwrap();
+}
